a12ca74cb14cdace95b60e4b6b6a18db28dace85
Krishna Wahi <krishna@clearly.earth>
2024-10-11 11:49:13 +0530

    Covered 4th step "support different delimeters"

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,14 +1,23 @@
-function stringCalculator(numbersStr) {
-    if (numbersStr === "") return 0;
+function stringCalculator(inputString) {
+    if (inputString === "") return 0;
+    let delimiter = /[,\n]/;
+    let numbersStr = inputString
+
+    // Check for custom delimiter format: "//[delimiter]\n[numbers...]"
+    if (inputString.startsWith("//")) {
+        const delimiterEndIdx = inputString.indexOf("\n");
+        delimiter = new RegExp(inputString.substring(2, delimiterEndIdx)); // Extracting custom delimiter
+        numbersStr = inputString.substring(delimiterEndIdx + 1); // Skipping custom delimiter
+    }
 
     // Split by comma and newline using regex
-    const numbers = numbersStr.split(/[\n,]/);
+    const numbers = numbersStr.split(delimiter);
     const negatives = [];
 
     // Calculating sum
     const sum = numbers.reduce((total, number) => {
         if (isNaN(number) || number === "") return total;
-        
+
         const parsedNumber = parseInt(number)
         if (parsedNumber < 0) {
             negatives.push(parsedNumber);
@@ -29,6 +38,7 @@ function stringCalculator(numbersStr) {
 console.log(stringCalculator("")); // Output: 0
 console.log(stringCalculator("1,3")); // Output: 4
 console.log(stringCalculator("1\n2,3")); // Output: 6
+console.log(stringCalculator("//;\n1;2")); // Output: 3
 
 // Throws: Negative numbers not allowed: -2
 console.log(stringCalculator("1,-2,3")); 
